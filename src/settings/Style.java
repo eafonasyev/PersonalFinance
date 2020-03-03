a3113017b87650ac87ff077cf65d4453b8168d0d
EAfonasyev <eafonasyev@gmail.com>
2020-03-03 19:13:21 +0600

    фаил стиля

---
+++ src/settings/Style.java
@@ -0,0 +1,24 @@
+package settings;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class Style {
+    public static final Color COLOR_BOTTON_NORMAL = new Color(240, 240, 240, 240);
+    public static final Color COLOR_BOTTON_HOVER = Color.green;
+
+    public static final Font FONT_BOTTON_TOOLBAR = new Font("Roboto-ligth", Font.BOLD, 14);
+    public static final Font FONT_BOTTON_MAIN = new Font("Roboto-ligth", Font.BOLD, 14);
+
+    public static final EmptyBorder EMPTY_BORDER = new EmptyBorder(10, 10, 10, 10);
+
+    public static final ImageIcon ICON_MAIN = new ImageIcon("image/icon_main.png");
+    public static final ImageIcon ICON_TOOLBAR_OVERVIEW = new ImageIcon("image/icon_toolbar_overview.png");
+    public static final ImageIcon ICON_TOOLBAR_ACTICLES = new ImageIcon("image/icon_toolbar_articles.png");
+    public static final ImageIcon ICON_TOOLBAR_STATISTICS = new ImageIcon("image/icon_toolbar_statistics.png");
+    public static final ImageIcon ICON_TOOLBAR_TRANSACTIONS = new ImageIcon("image/icon_toolbar_transactions.png");
+    public static final ImageIcon ICON_TOOLBAR_ACCOUNTS = new ImageIcon("image/icon_toolbar_accounts.png");
+    public static final ImageIcon ICON_TOOLBAR_CURRENCIES = new ImageIcon("image/icon_toolbar_currencies.png");
+
+}
